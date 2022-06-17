823c67edbb58976d4c5f783d626e0dc774d72947
evgenydmc <darkonebadger@gmail.com>
2022-06-17 22:46:37 +0300

    Solve first problem

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -2,13 +2,12 @@ def concatenate_strings(a: str, b: str) -> str:
     """
     Функция для сложения двух строк.
     Результат сложения запишите в переменную result.
-
     :param a: число
     :param b: число
     :return: результат сложения
     """
 
-    # пиши свой код здесь
+    result = a + b
 
     return result
 
@@ -19,9 +18,10 @@ def calculate_salary(total_compensation: int) -> float:
     вычета налогов. Ставка налогообложения равна 13%.
 
     :param total_compensation: сумма зарплаты до вычета налога
+
     :return: сумма заплаты после вычета налога
     """
 
     # пиши свой код здесь
-
+    result = total_compensation * 0,87
     return result
